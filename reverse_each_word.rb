91b2e485181752f81125148ef747e9ada3c95ee2
Kimberly Aviles <01010110.v@gmail.com>
2018-11-08 18:57:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -16,6 +16,6 @@ end
 # method using .collect - (refactored)
 def reverse_each_word(sentence)
   sentence = sentence.split(" ")
-  sentence.collect { |word| word.reverse }
+  sentence.collect! { |word| word.reverse }
   sentence = sentence.join(" ")
 end
